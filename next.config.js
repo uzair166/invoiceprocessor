4d5bc4e6a27b86d2599dbd216b21b4566754d22a
Uzair Ishaq <uzair.i@hotmail.co.uk>
2024-12-05 10:56:34 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // Required for pdf-parse
     return config;
